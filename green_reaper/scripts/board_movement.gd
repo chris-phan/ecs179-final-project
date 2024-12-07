bf48171b8aefa2c0feacc75758e56eaf295e07af
Hanson <Hansonlau7@gmail.com>
2024-12-07 00:55:51 -0800

    board art finished, dice animation added, camera feature changed (zoomed out camera is now moveable and is bounded), dice roll press space key, move map with arrow keys or wasd

--- green_reaper/scripts/board_movement.gd
+++ green_reaper/scripts/board_movement.gd
@@ -14,15 +14,20 @@ var _current_space: int = 0 # index of space
 var _next_space: int = 1 # index of next space
 var _current_animation: String = "idle"
 var space_indicators: Array[String] = []
+var dice_rolling_done: bool
+var moving_done: bool
 
 signal space_landed(space_type)
 
 
 func _ready() -> void:
+	dice_rolling_done = false
+	moving_done = true
+	z_index = 5
 	# go to idle animation
 	animation_player.play("idle")
 	board.connect("board_setup_done", _on_board_setup_done)
-	z_index = 5
+	dice.connect("dice_done_waiting", set_dice_rolling_done)
 	
 
 func _on_board_setup_done() -> void:
@@ -52,52 +57,59 @@ func _on_board_setup_done() -> void:
 func _process(_delta: float) -> void:
 	
 	# something trigger dice roll (for now it's 'd')
-	if Input.is_action_just_pressed("roll_dice"):
-		#print("d pressed")
+	if moving_done and Input.is_action_just_pressed("roll_dice"):
+		# print("d pressed")
+		moving_done = false
 		_moves = dice.roll_dice()
 		print("dice: " + str(_moves))
 
-	# after dice roll
-	
-	if _moves > 0:
-		if _current_space == len(board_vectors) - 1:
-			_next_space = 0
-		
-		if position.x < board_vectors[_next_space].x:
-			position.x += _delta * _player_speed
-			if position.x >= board_vectors[_next_space].x:
-				position.x = board_vectors[_next_space].x
-			# walking right
-			animation_player.play("right")
-		elif position.x > board_vectors[_next_space].x:
-			position.x -= _delta * _player_speed
-			if position.x <= board_vectors[_next_space].x:
-				position.x = board_vectors[_next_space].x
-			# walking left
-			animation_player.play("left")
-		elif position.y > board_vectors[_next_space].y:
-			position.y -= _delta * _player_speed
-			if position.y < board_vectors[_next_space].y:
-				position.y = board_vectors[_next_space].y
-			# walking up
-			animation_player.play("up")
-		elif position.y < board_vectors[_next_space].y:
-			position.y += _delta * _player_speed
-			if position.y > board_vectors[_next_space].y:
-				position.y = board_vectors[_next_space].y
-			# walking down
-			animation_player.play("down")
-		else:
-			# destination reached
-			_moves -= 1
-			_current_space = _next_space
-			position = board_vectors[_current_space];
-			_next_space += 1
-			animation_player.play("idle")
-			#print("move done")
+	if dice_rolling_done:
+		# after dice roll
+		if _moves > 0:
+			if _current_space == len(board_vectors) - 1:
+				_next_space = 0
 			
-			# check if it's last move
-			if _moves == 0:
-				# check which space it is on an emit signal depending on it.
-				emit_signal("space_landed", space_indicators[_current_space])
-				print("landed on " + space_indicators[_current_space] + " space")
+			if position.x < board_vectors[_next_space].x:
+				position.x += _delta * _player_speed
+				if position.x >= board_vectors[_next_space].x:
+					position.x = board_vectors[_next_space].x
+				# walking right
+				animation_player.play("right")
+			elif position.x > board_vectors[_next_space].x:
+				position.x -= _delta * _player_speed
+				if position.x <= board_vectors[_next_space].x:
+					position.x = board_vectors[_next_space].x
+				# walking left
+				animation_player.play("left")
+			elif position.y > board_vectors[_next_space].y:
+				position.y -= _delta * _player_speed
+				if position.y < board_vectors[_next_space].y:
+					position.y = board_vectors[_next_space].y
+				# walking up
+				animation_player.play("up")
+			elif position.y < board_vectors[_next_space].y:
+				position.y += _delta * _player_speed
+				if position.y > board_vectors[_next_space].y:
+					position.y = board_vectors[_next_space].y
+				# walking down
+				animation_player.play("down")
+			else:
+				# destination reached
+				_moves -= 1
+				_current_space = _next_space
+				position = board_vectors[_current_space];
+				_next_space += 1
+				#print("move done")
+				
+				# check if it's last move
+				if _moves == 0:
+					animation_player.play("idle")
+					# check which space it is on an emit signal depending on it.
+					emit_signal("space_landed", space_indicators[_current_space])
+					print("landed on " + space_indicators[_current_space] + " space")
+					dice_rolling_done = false
+					moving_done = true
+
+
+func set_dice_rolling_done() -> void:
+	dice_rolling_done = true
--- green_reaper/scripts/camera_base.gd
+++ green_reaper/scripts/camera_base.gd
@@ -1,31 +1,60 @@
 class_name CameraBase
 extends Camera2D
 
-@export var zoom_out: float = 0.8
+@export var zoom_out: float = 1.75
 @export var zoom_in: float = 3.0
+@export var zoom_out_move_speed: int = 250
+
+const MAP_X_MAX = 512
+const MAP_X_MIN = -512
+const MAP_Y_MAX = -234
+const MAP_Y_MIN = 345
 
 var _board_player: CharacterBody2D
 var _camera_on_player: bool = false
+var _zoom_out_x: int
+var _zoom_out_y: int
 
 
 func _ready() -> void:
 	_board_player = %BoardPlayer
-	position = Vector2(0, 0)
-	zoom = Vector2(zoom_out, zoom_out)
+	_zoom_out_x = _board_player.position.x
+	_zoom_out_y = _board_player.position.y
+	zoom_in_camera()
 
 
 func _process(delta: float) -> void:
 	# press 'z' key
 	if Input.is_action_just_pressed("zoom_toggle"):
 		if (not _camera_on_player):
-			zoom = Vector2(zoom_in, zoom_in)
+			zoom_in_camera()
 		else:
-			zoom = Vector2(zoom_out, zoom_out)
+			zoom_out_camera()
 
-		_camera_on_player = not _camera_on_player
-	
 	if (_camera_on_player):
 		position = _board_player.position
 	else:
-		position = Vector2(0, 0)
+		position = Vector2(_zoom_out_x, _zoom_out_y)
+		
+		if Input.is_action_pressed("map_up"):
+			_zoom_out_y = max(MAP_Y_MAX, (_zoom_out_y - (delta * zoom_out_move_speed)))
+		if Input.is_action_pressed("map_down"):
+			_zoom_out_y = min(MAP_Y_MIN, (_zoom_out_y + (delta * zoom_out_move_speed)))
+		if Input.is_action_pressed("map_right"):
+			_zoom_out_x = min(MAP_X_MAX, (_zoom_out_x + (delta * zoom_out_move_speed)))
+		if Input.is_action_pressed("map_left"):
+			_zoom_out_x = max(MAP_X_MIN, (_zoom_out_x - (delta * zoom_out_move_speed)))
+
+
+func zoom_in_camera() -> void:
+	position = _board_player.position
+	zoom = Vector2(zoom_in, zoom_in)
+	_camera_on_player = true
+
+
+func zoom_out_camera() -> void:
+	_zoom_out_x = _board_player.position.x
+	_zoom_out_y = _board_player.position.y
+	zoom = Vector2(zoom_out, zoom_out)
+	_camera_on_player = false
 	
