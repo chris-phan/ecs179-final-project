902c6b311bec33bfeb949eec7d91f485653719c4
Aditya-Bhatia <adityab.2610@gmail.com>
2024-12-09 01:01:52 -0800

    Boss runs once every 5 turns

--- green_reaper/scripts/minigame_manager.gd
+++ green_reaper/scripts/minigame_manager.gd
@@ -73,8 +73,8 @@ func _handle_enter_minigame() -> void:
 		minigame_rotation.append(ObservationMinigame.new())
 		minigame_rotation.shuffle()
 
-	var boss_phase = state_manager.turns_passed / 1 - 1
-	if state_manager.turns_passed % 1 == 0 and state_manager.cash < (75000 + 25000 * boss_phase):
+	var boss_phase = state_manager.turns_passed / 5 - 1
+	if state_manager.turns_passed % 5 == 0 and state_manager.cash < (75000 + 25000 * boss_phase):
 		minigame_rotation.clear()
 		minigame_rotation.append(BossMinigame.new())
 		difficulty = boss_phase as Minigame.Difficulty
